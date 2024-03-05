a893f167bc22a6a2ba9aa8142554d2cf0bb865a6
Onur Akdoğan <55892556+onur-akdogan@users.noreply.github.com>
2024-03-05 12:40:44 +0300

    Update 2024_03_04_190553_create_payment_table.php

--- database/migrations/2024_03_04_190553_create_payment_table.php
+++ database/migrations/2024_03_04_190553_create_payment_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('payment', function (Blueprint $table) {
+        Schema::create('payment', function (Blueprint $table) { 
             $table->id();
             $table->text("bankname");
             $table->text("username");
